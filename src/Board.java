fc34bb6b4add3933f8baf1f1e72d4f9276dbe5b2
Victor Ruiz <vgruiz@Victors-Mac-mini.local>
2019-03-09 00:52:47 -0800

    Coding up the Board class

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,150 @@
+import java.util.Random;
 
 public class Board {
-	Queen[][] gameBoard;
+//	Queen[][] gameBoard;
+	int[] queens;
+	boolean[] attackStatus;
+	int n;
+	Random random = new Random();
 	
-	public Board() {
-		gameBoard = new Queen[8][8];
+	public Board(int n) {
+		//gameBoard = new Queen[8][8];
+		queens = new int[n];
+		attackStatus = new boolean[n];
+		this.n = n;
+		setInitialPositions();
+	}
+	
+	public void setInitialPositions() {
+		for(int i = 0; i < queens.length; i++) {
+			queens[i] = random.nextInt(n);
+		}
+	}
+	
+	public void print() {
+		for(int j = 0; j < n; j++) {
+			for(int i = 0; i < n; i++) {
+				if(i == n - 1 && queens[i] == j) {
+					System.out.println(" X ");
+				} else if(i == n - 1) {
+					System.out.println(" | ");
+				} else if(queens[i] == j) {
+					System.out.print(" X ");
+				} else {
+					System.out.print(" | ");					
+				}
+				
+			}
+		}
+	}
+	
+	//TODO: Finish this lol
+	public void generateRandomSuccessor() {
+		int randCol = random.nextInt(n);
+		int randRow = random.nextInt(n);
 		
+		queens[randCol] = randRow;
 		
+	}
+	
+	/**
+	 * 
+	 * @param column
+	 * @returns true if it is in an attack position, if it is in the same row or diagonal as another queen
+	 */
+	public boolean isAttacked(int column) {
+		int col = column;
+		int row = queens[column];
+		
+		for(int i = 0; i < queens.length; i++) {
+			if(i == col && queens[i] == row) {
+				continue;
+			}
+			
+			//checking for horizontals
+			if(queens[i] == row) {
+				//System.out.println("Horizontal @ "+ queens[i] + ", " + i + " and " + row +", " + col);
+				return true;
+			}
+			
+			//checking for diagonals
+			int colDiff = Math.abs(i - col);
+			int rowDiff = Math.abs(queens[i] - row);
+			if (rowDiff == colDiff) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public int getNumberOfAttackedQueens() {
+		int count = 0;
+		
+		for(int i = 0; i < queens.length; i++) {
+			if(attackStatus[i] == false) {
+				for(int j = 0; j < queens.length; j++) {
+					if(i == j) {
+						continue;
+					}					
+					//checking for horizontals
+					if(queens[i] == queens[j]) {
+						
+					}
+					//checking for diagonals
+					int colDiff = Math.abs(i - j);
+					int rowDiff = Math.abs(queens[i] - queens[j]);
+					if (rowDiff == colDiff) {
+						
+					}
+				}
+				
+			}
+			
+			
+//			
+//			if(i == col && queens[i] == row) {
+//				continue;
+//			}
+//			
+//			//checking for horizontals
+//			if(queens[i] == row) {
+//				//System.out.println("Horizontal @ "+ queens[i] + ", " + i + " and " + row +", " + col);
+//				return true;
+//			}
+//			
+//			//checking for diagonals
+//			int colDiff = Math.abs(i - col);
+//			int rowDiff = Math.abs(queens[i] - row);
+//			if (rowDiff == colDiff) {
+//				return true;
+//			}
+		}
+	}
+	
+	public void printStatus() {
+		for(int i = 0; i < queens.length; i++) {
+			System.out.println(queens[i] + " " + i);
+		}
+		
+		for(int i = 0; i < queens.length; i++) {
+			for(int j = 0; j < queens.length; j++) {
+				if(i == j && queens[i] == queens[j]) {
+					continue;
+				}
+				
+				if(queens[i] == queens[j]) {
+					System.out.println("Horizontal @ "+ queens[i] + ", " + i + " and " + queens[j]+", " + j);
+				}
+				
+				//checking for diagonals
+				int colDiff = Math.abs(i - j);
+				int rowDiff = Math.abs(queens[i] - queens[j]);
+				if (rowDiff == colDiff) {
+					System.out.println("Diagonal @ "+ queens[i] + ", " + i + " and " + queens[j]+", " + j);
+				}
+			}
+		}
 	}
 	
 	public void isSolved() {}
--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,49 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Board board = new Board(4);
+		board.print();
+		board.printStatus();
 	}
 
-	public SolutionState SimulatedAnnealing(Problem problem, Schedule schedule) {
-		/**
-		 * 	problem, a problem
-		 * 	schedule, a mapping from time to "temperature"
-		 * 
-		 */
-		
-		Board currentBoard;
+	public void SimulatedAnnealing(Board board) {
+		Board currentBoard = board;
 		Board nextBoard;
-		int T; //a "temperature" controlling probability of downward steps
-				// maybe controlling how many queens are moving on a given turn
 		
-		int deltaE;
 		
-		currentBoard = MakeNode(InitialState(problem));
-		
-		for(int t = 0; t < 1000000; t++) {
-			T = schedule[t];
-			if(T = 0) {
-				return currentBoard;
-			}
-			
-			deltaE = Value(nextBoard) - Value(currentBoard);
-			
-			if (deltaE > 0) {
-				currentBoard = nextBoard;
-			} else {
-				currentBoard = nextBoard;
-				//"only with probability e^(deltaE/T)
-			}
-		}
 	}
+	
+//	public SolutionState SimulatedAnnealing(Problem problem, Schedule schedule) {
+//		/**
+//		 * 	problem, a problem
+//		 * 	schedule, a mapping from time to "temperature"
+//		 * 
+//		 */
+//		
+//		Board currentBoard;
+//		Board nextBoard;
+//		int T; //a "temperature" controlling probability of downward steps
+//				// maybe controlling how many queens are moving on a given turn
+//		
+//		int deltaE;
+//		
+//		currentBoard = MakeNode(InitialState(problem));
+//		
+//		for(int t = 0; t < 1000000; t++) {
+//			T = schedule[t];
+//			if(T = 0) {
+//				return currentBoard;
+//			}
+//			
+//			deltaE = Value(nextBoard) - Value(currentBoard);
+//			
+//			if (deltaE > 0) {
+//				currentBoard = nextBoard;
+//			} else {
+//				currentBoard = nextBoard;
+//				//"only with probability e^(deltaE/T)
+//			}
+//		}
+//	}
 }
 
